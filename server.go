09a52e874feca225ccb3a7b529a5b2a9214518c3
abarbaro <abarbaro@redhat.com>
2023-03-22 16:27:28 +0100

    second commit

--- server.go
+++ server.go
@@ -9,7 +9,7 @@ import (
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("got / request\n")
-	fmt.Fprintf(w, "hello\n")
+	fmt.Fprintf(w, "hello everyone\n")
 }
 
 func version(w http.ResponseWriter, req *http.Request) {
